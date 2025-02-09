24f2e1ced95b4b730729fea7b15e38c2c17ec98e
Lukasz Burchard <luki446@gmail.com>
2025-02-10 00:22:25 +0100

    Use simpler camera movement system

--- src/player.cpp
+++ src/player.cpp
@@ -1,4 +1,5 @@
 #include "player.hpp"
+#include "raylib.h"
 
 namespace Vox {
 
@@ -11,22 +12,7 @@ Player::Player(World& world) : main_world(world) {
 }
 
 void Player::update() {
-  UpdateCameraPro(&this->camera_impl,
-                  (Vector3){
-                      (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) *
-                              0.1f -  // Move forward-backward
-                          (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * 0.1f,
-                      (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) *
-                              0.1f -  // Move right-left
-                          (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * 0.1f,
-                      0.0f  // Move up-down
-                  },
-                  (Vector3){
-                      GetMouseDelta().x * 0.05f,  // Rotation: yaw
-                      GetMouseDelta().y * 0.05f,  // Rotation: pitch
-                      0.0f                        // Rotation: roll
-                  },
-                  GetMouseWheelMove() * 0.9f);
+    UpdateCamera(&this->camera_impl, CAMERA_FIRST_PERSON);
 }
 
 }  // namespace Vox
